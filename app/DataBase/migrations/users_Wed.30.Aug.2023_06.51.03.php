46f15b8c70034eb39eaa568fafd00cbf0d2e1db7
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-09-04 00:27:37 -0600

    Create user default; user: admin@a.con password: 123

--- app/DataBase/migrations/users_Wed.30.Aug.2023_06.51.03.php
+++ app/DataBase/migrations/users_Wed.30.Aug.2023_06.51.03.php
@@ -3,15 +3,26 @@ class users extends Migration {
         
     public function up() {
         $this->create("users", function($table) {
+            $this->prepare();
             $this->query('CREATE TABLE users (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 email VARCHAR(255) NOT NULL UNIQUE,
                 name VARCHAR(100) NOT NULL,
                 user VARCHAR(255) NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                remember_token VARCHAR(255) NULL,
+                remember_token VARCHAR(255),
+                avatar_serve VARCHAR(255) DEFAULT "' . serve('localhost:8080') . '",
+                avatar_rute VARCHAR(255) DEFAULT "assets/images/panel/undraw_profile_2.svg",
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )');            
+            )');
+            import('Encrypt/hasher.php', false, '/core');
+            $this->insert('users')->values([
+                'email' => 'admin@a.com',
+                'password' => Hasher::make('123'),
+                'name' => 'Admin',
+                'user' => 'Admin'
+            ]);
+            $this->execute();
         });
     }
         
