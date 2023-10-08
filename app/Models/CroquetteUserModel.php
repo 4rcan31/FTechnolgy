07965fae4bd472876ed1d66d5debf9cea253b378
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-10-07 23:54:19 -0600

    Add new methods in Croquete_User Modal

--- app/Models/CroquetteUserModel.php
+++ app/Models/CroquetteUserModel.php
@@ -54,4 +54,29 @@ class CroquetteUserModel extends BaseModel{
         $this->select(['*'])->from($this->table)->where('id_user', $idUser);
         return $this->execute()->all('fetchAll');
     }
+
+    public function getStateConnectionByIdCroquette(int $idCroquette){
+        $this->prepare();
+        $this->select(["state"])->from('croquette_user')->where('id_croquette', $idCroquette);
+        $response = $this->execute()->all();
+        return $response && isset($response->state) ? $response->state === 1 : false;
+    }
+
+
+    /*
+        Esta consulta se considera delicada y demandante en términos de rendimiento, 
+        ya que actualiza todos los registros de la tabla en la columna "state" de la 
+        tabla "croquetes_users". Sin embargo, esta consulta solo se ejecuta en circunstancias excepcionales, 
+        es decir, cuando el servidor de comunicación de croquettes se desconecta o experimenta una interrupción. 
+        En tales casos, se requiere esta actualización para cambiar el estado de todos los croquettes
+        previamente conectados a "desconectados".
+        Esta consulta se encuentra en la línea 333 (en el momento actual) del método 'setStateOffAllCroquettes' en el archivo 'CroqueteController.php'.
+    */
+    public function setStateOffAllCroquettes(){
+        $this->prepare(); 
+        $this->update($this->table, [
+            'state'  => 0
+        ]);
+        return $this->execute();
+    }
 }
