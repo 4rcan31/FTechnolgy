9e0f329d9a59b22ca6a8185d4c62a29040fa7941
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-11-18 00:10:29 -0600

    testing croquette .ino software

--- Croquette/clientnew.ino
+++ Croquette/clientnew.ino
@@ -3,26 +3,23 @@
 #include <LiquidCrystal_I2C.h>
 #include <HX711.h>
 
-// VARIABLES & PINS
-// Stepper Motor
-#define STEPS 32
-int Steps2Take;
-const int X_Speed = 600;
+// Define constants for pins
 const int In1 = 2, In2 = 4, In3 = 3, In4 = 5;
-Stepper small_stepper(STEPS, In1, In2, In3, In4);
-
-// Buttons
 const int S_pin = 6;
 const int E_pin = 7;
-int S_state = 0;
-int E_state = 0;
+const int DTPin = A0, SCKPin = A1;
 
+// Stepper motor configuration
+const int STEPS = 32;
+const int X_Speed = 600;
+Stepper small_stepper(STEPS, In1, In2, In3, In4);
+
+// LCD configuration
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Load Cell
+// Load cell configuration
 HX711 scale;
-const int DTPin = A0, SCKPin = A1;
-const int scale_factor = 420.0983;
+const float scale_factor = 420.0983;
 float Weight;
 int Qty_Weight = 45;
 int Cust_Qty_Weight = 0;
@@ -36,10 +33,8 @@ const int MTimerErr = 30;
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(S_pin, INPUT_PULLUP);
   pinMode(E_pin, INPUT_PULLUP);
-
   Wire.begin();
 
   lcd.init();
@@ -55,15 +50,18 @@ void setup() {
   lcd.clear();
   delay(500);
 
-//lcd.setCursor(0, 0);
-  //lcd.print(" /\\_/\\ ");
-  //lcd.setCursor(0, 1);
-  //lcd.print("( o.o )");
-  
-  //delay(3000);
+  // Display cat ASCII art
+  lcd.setCursor(0, 0);
+  lcd.print(" /\\_/\\ ");
+  lcd.setCursor(0, 1);
+  lcd.print("( o.o )");
+  delay(3000);
 
-  // Main Menu
-  Mainmenu:
+  setupMainMenu();
+}
+
+void setupMainMenu() {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(">");
   lcd.setCursor(1, 0);
@@ -75,81 +73,26 @@ void setup() {
   lcd.setCursor(1, 1);
   lcd.print("Custom:");
 
-  do {
-    S_state = digitalRead(S_pin);
-    E_state = digitalRead(E_pin);
-    if (S_state == LOW) {
-      lcd.setCursor(0, Sel_Line);
-      lcd.print(" ");
-      if (Sel_Line == 0)
-        Sel_Line = Sel_Line + 1;
-      else
-        Sel_Line = Sel_Line - 1;
+  while (digitalRead(E_pin) == HIGH) {
+    int buttonState = digitalRead(S_pin);
+    if (buttonState == LOW) {
+      Sel_Line = 1 - Sel_Line;
       lcd.setCursor(0, Sel_Line);
       lcd.print(">");
       delay(300);
     }
-  } while (E_state == HIGH);
-  delay(300);
-
-  if (Sel_Line == 0)
-    goto Endsetup;
-  if (Sel_Line == 1) {
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Quantity:");
-    lcd.setCursor(1, 1);
-    lcd.print(Cust_Qty_Weight);
-    lcd.print(" g");
-
-    do {
-      S_state = digitalRead(S_pin);
-      E_state = digitalRead(E_pin);
-      if (S_state == LOW) {
-        if (Cust_Qty_Weight < 95)
-          Cust_Qty_Weight = Cust_Qty_Weight + 5;
-        else
-          Cust_Qty_Weight = 5;
-        lcd.setCursor(1, 1);
-        lcd.print("  ");
-        lcd.setCursor(1, 1);
-        lcd.print(Cust_Qty_Weight);
-        Qty_Weight = Cust_Qty_Weight;
-        delay(300);
-      }
-    } while (E_state == HIGH);
-    delay(300);
-
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Feed Interval:");
-    lcd.setCursor(1, 1);
-    lcd.print(Cust_InterV);
-    lcd.print(" hr");
-
-    do {
-      S_state = digitalRead(S_pin);
-      E_state = digitalRead(E_pin);
-      if (S_state == LOW) {
-        if (Cust_InterV < 23)
-          Cust_InterV = Cust_InterV + 1;
-        else
-          Cust_InterV = 1;
-        lcd.setCursor(1, 1);
-        lcd.print("  ");
-        lcd.setCursor(1, 1);
-        lcd.print(Cust_InterV);
-        InterV = Cust_InterV;
-        delay(300);
-      }
-    } while (E_state == HIGH);
-    delay(300);
-
-    Sel_Line = 0;
-    goto Mainmenu;
   }
 
-  Endsetup:
+  delay(300);
+
+  if (Sel_Line == 0) {
+    // Default settings
+    goto endSetup;
+  } else if (Sel_Line == 1) {
+    setupCustomSettings();
+  }
+
+endSetup:
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Miam!");
@@ -158,12 +101,71 @@ void setup() {
   delay(Delaystart * 1000);
 }
 
+void setupCustomSettings() {
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Quantity:");
+  lcd.setCursor(1, 1);
+  lcd.print(Cust_Qty_Weight);
+  lcd.print(" g");
+
+  while (digitalRead(E_pin) == HIGH) {
+    int buttonState = digitalRead(S_pin);
+    if (buttonState == LOW) {
+      Cust_Qty_Weight = (Cust_Qty_Weight < 95) ? Cust_Qty_Weight + 5 : 5;
+      lcd.setCursor(1, 1);
+      lcd.print("  ");
+      lcd.setCursor(1, 1);
+      lcd.print(Cust_Qty_Weight);
+      Qty_Weight = Cust_Qty_Weight;
+      delay(300);
+    }
+  }
+
+  delay(300);
+
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Feed Interval:");
+  lcd.setCursor(1, 1);
+  lcd.print(Cust_InterV);
+  lcd.print(" hr");
+
+  while (digitalRead(E_pin) == HIGH) {
+    int buttonState = digitalRead(S_pin);
+    if (buttonState == LOW) {
+      Cust_InterV = (Cust_InterV < 23) ? Cust_InterV + 1 : 1;
+      lcd.setCursor(1, 1);
+      lcd.print("  ");
+      lcd.setCursor(1, 1);
+      lcd.print(Cust_InterV);
+      InterV = Cust_InterV;
+      delay(300);
+    }
+  }
+
+  delay(300);
+  Sel_Line = 0;
+  setupMainMenu();
+}
+
 void loop() {
   Start_MTimer = millis() / 1000;
+  FeedLoop();
 
-  FeedLoop:
+FeedComplete:
+  Serial.print("Complete");
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, LOW);
+  digitalWrite(In3, LOW);
+  digitalWrite(In4, LOW);
+
+  delay(InterV * 1000);
+}
+
+void FeedLoop() {
   for (int x = 0; x < 4; x++) {
-    Steps2Take = 500;
+    int Steps2Take = 500;
     small_stepper.setSpeed(X_Speed);
     small_stepper.step(Steps2Take);
     Weight = scale.get_units();
@@ -172,10 +174,12 @@ void loop() {
     Serial.println(Weight);
     Serial.print("Motor Time: ");
     Serial.println(Stop_MTimer - Start_MTimer);
-    if (Weight > Qty_Weight)
+    if (Weight > Qty_Weight) {
       goto FeedComplete;
-    if (Stop_MTimer - Start_MTimer > MTimerErr)
+    }
+    if (Stop_MTimer - Start_MTimer > MTimerErr) {
       goto FeedError;
+    }
   }
 
   delay(500);
@@ -185,10 +189,10 @@ void loop() {
   small_stepper.step(Steps2Take);
 
   delay(500);
-
   goto FeedLoop;
+}
 
-  FeedError:
+void FeedError() {
   digitalWrite(In1, LOW);
   digitalWrite(In2, LOW);
   digitalWrite(In3, LOW);
@@ -198,20 +202,22 @@ void loop() {
   lcd.setCursor(5, 0);
   lcd.print("ERROR");
 
-  E_state = digitalRead(E_pin);
+  int E_state = digitalRead(E_pin);
   while (E_state == HIGH) {
     for (int light_on = 0; light_on < 501; light_on++) {
       E_state = digitalRead(E_pin);
-      if (E_state == LOW)
+      if (E_state == LOW) {
         break;
+      }
       lcd.backlight();
       delay(1);
     }
 
     for (int light_off = 0; light_off < 501; light_off++) {
       E_state = digitalRead(E_pin);
-      if (E_state == LOW)
+      if (E_state == LOW) {
         break;
+      }
       lcd.noBacklight();
       delay(1);
     }
@@ -224,7 +230,7 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print("Refill & Press OK");
 
-  E_state = digitalRead(E_pin);
+  int E_state = digitalRead(E_pin);
   while (E_state == HIGH) {
     E_state = digitalRead(E_pin);
   }
@@ -232,16 +238,5 @@ void loop() {
 
   lcd.noBacklight();
   Start_MTimer = millis() / 1000;
-  goto FeedLoop;
-
-  FeedComplete:
-  Serial.print("Complete");
-  digitalWrite(In1, LOW);
-  digitalWrite(In2, LOW);
-  digitalWrite(In3, LOW);
-  digitalWrite(In4, LOW);
-
-  delay(InterV * 1000);
-
-  // Wait until the next interval (in hours) before the next loop
+  FeedLoop();
 }
---
+++ Croquette/helpers.ino
@@ -0,0 +1,4 @@
+unsigned long hoursToMilliseconds(int hours) {
+  unsigned long milliseconds = hours * 3600000UL;  // 3600000 milliseconds in an hour
+  return milliseconds;
+}
